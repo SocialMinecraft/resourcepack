c7fdd84ce4d5a47917a25868a316256287916647
Dylan Garvis <dylan@garvis.dev>
2023-08-16 17:08:25 -0400

    Fixed bug in bundle script.

--- bundle.sh
+++ bundle.sh
@@ -3,5 +3,5 @@
 mkdir -p bundles
 
 archive_name="bundles/SoMC-$(date '+%Y%m%d-%H%M').zip"
-zip $archive_name assets pack.mcmeta pack.png
+zip -r $archive_name assets pack.mcmeta pack.png
 shasum $archive_name > "$archive_name.hash"
